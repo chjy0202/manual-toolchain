9cf4d0afc938939d2fa367fcad695c8f0ca457bf
chjy0202 <chjy0202@naver.com>
2022-11-06 01:00:55 +0900

    conf: JSX 설정
    - 현재 상태에서는 JSX 가 웹팩 번들링 과정에서 컴파일되지 않아 해석되지 않으므로 오류가 발생

--- webpack/common.js
+++ webpack/common.js
@@ -2,9 +2,13 @@ import { resolve } from 'node:path';
 
 const commonConfig = {
   target: ['browserslist'],
+  // .jsx 확장자 없이도 불러올 수 있도록 수정
+  resolve: {
+    extensions: ['.js', '.jsx', '.json', '.wasm'],
+  },
   entry: {
     // 절대경로 처리 (join은 상대경로 처리)
-    main: resolve('src/main.js'),
+    main: resolve('src/main.jsx'),
   },
   output: {
     path: resolve('public'),
