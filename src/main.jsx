2db605c10373f11c222e4647fed6d16be4af59ba
chjy0202 <chjy0202@naver.com>
2022-11-09 14:57:14 +0900

    conf: render 컴포넌트 분리
    - App.jsx
    - Link.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
-const App = () => (
-  <figure className="react-figure">
-    <a
-      className="link"
-      href="https://reactjs.org"
-      rel="noopener noreferrer"
-      target="_blank"
-    >
-      <img className="logo" src="assets/react.svg" alt="React" />
-    </a>
-    <figcaption className="description">React 툴체인 매뉴얼 구성</figcaption>
-  </figure>
-);
-
 const container = document.getElementById('root');
 const root = createRoot(container);
 
