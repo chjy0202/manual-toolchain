940bbf41e30673d6e181a9590a0b2558fa7dc5f3
chjy0202 <chjy0202@naver.com>
2022-11-09 22:13:58 +0900

    fix: App 컴포넌트 누락 수정

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import App from './app/App';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
